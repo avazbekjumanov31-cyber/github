acb5dcba62fec53aa9c61a8629df1dd92e1705c8
avazbek <avazbekjumanov31-cyber>
2026-08-25 21:23:52 -0700

    yangolash

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Home() {
+    const  [product, setproduct]= useState();
+
+    const getProduct = async () => {
+      const res = await fetch("http://localhost:3000/product");
+      const datac = res.json();
+
+      setproduct(data);
+    };
+
+    useEffect(()=>{
+      getProduct();
+    },[]);
   return (
-    <div>Home</div>
+  
+  <>
+     <div>
+      {product.map((product)=>(
+
+      ))}
+     </div>
+  </>
   )
 }
 
